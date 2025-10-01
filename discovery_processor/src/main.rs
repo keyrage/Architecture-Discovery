07c202a46e20b7957478642df373860154900557
Craig Beetlestone <craig@beetlestone.nz>
2025-10-01 19:22:00 +1300

    discovery processor

--- discovery_processor/src/main.rs
+++ discovery_processor/src/main.rs
@@ -1,3 +1,75 @@
-fn main() {
-    println!("Hello, world!");
+
+use rdkafka::client::ClientContext;
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::{Consumer, ConsumerContext};
+use rdkafka::error::KafkaResult;
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::topic_partition_list::TopicPartitionList;
+use rdkafka::util::get_rdkafka_version;
+use rdkafka::Message;
+use log::*;
+
+struct LoggingConsumerContext;
+
+impl ClientContext for LoggingConsumerContext {}
+
+impl ConsumerContext for LoggingConsumerContext {
+    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
+        match result {
+            Ok(_) => info!("Offsets committed successfully"),
+            Err(e) => warn!("Error while committing offsets: {}", e),
+        };
+    }
+}
+
+type LoggingConsumer = StreamConsumer<LoggingConsumerContext>;
+
+fn create_consumer(brokers: &str, group_id: &str, topic: &str) -> LoggingConsumer {
+    let context = LoggingConsumerContext;
+
+    let consumer: LoggingConsumer = ClientConfig::new()
+        .set("group.id", group_id)
+        .set("bootstrap.servers", brokers)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        // Commit automatically every 5 seconds.
+        .set("enable.auto.commit", "true")
+        .set("auto.commit.interval.ms", "5000")
+        // but only commit the offsets explicitly stored via `consumer.store_offset`.
+        .set("enable.auto.offset.store", "false")
+        .set_log_level(RDKafkaLogLevel::Debug)
+        .create_with_context(context)
+        .expect("Consumer creation failed");
+
+    consumer
+        .subscribe(&[topic])
+        .expect("Can't subscribe to specified topic");
+
+    consumer
+}
+
+#[tokio::main]
+async fn main() {
+    let input_topic = "mqtt.agents";
+    let brokers = "localhost:9092";
+    let group_id = "discovery";
+    let consumer = create_consumer(brokers, group_id, input_topic);
+    println!("Starting");
+    loop {
+        match consumer.recv().await {
+            Err(e) => warn!("Kafka error: {}", e),
+            Ok(m) => {
+                let payload = match m.payload_view::<str>() {
+                    None => "",
+                    Some(Ok(s)) => s,
+                    Some(Err(e)) => {
+                        warn!("Error while deserialzing message payload {:?}", e);
+                        ""
+                    }
+                };
+                println!("{}",payload);
+            }
+        }
+    }
 }
